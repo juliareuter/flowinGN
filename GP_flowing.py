53cfb4388388f55936a68faf83054de208c26456
juliareu <julia.reuter@ovgu.de>
2023-03-13 16:01:21 +0100

    improved readability of code, replaced hard-coded parameter by variable n_neighbors

--- GP_flowing.py
+++ GP_flowing.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
 import pysr
-import argparse
 from sklearn.model_selection import train_test_split
 from pysr import PySRRegressor
 
 
-def giveData(Re, phi, spherical, n_neighbors, node_update, const_complexity):
+def giveData(Re, phi, spherical, n_neighbors, node_update):
     filename = f'Re{Re.replace(".","")}_phi{phi.replace(".","")}'
     if spherical == True:
         filename += '_spherical'
@@ -42,7 +41,7 @@ def giveData(Re, phi, spherical, n_neighbors, node_update, const_complexity):
 
     return X_train, y_train, F_nodeupdate_train, X_nodeupdate_train, X_test, y_test, F_test, X_nodeupdate_test, filename
 
-def runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations, run, const_complexity, model_selection, filename):
+def runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations, run, const_complexity, model_selection, filename, node_update, n_neighbors):
 
     #Define algorithm settings
     bin_operators       = ["+", "*", "/"]
@@ -51,13 +50,12 @@ def runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations
     comp_of_var         = 1
     constr              = {'cos': 7, 'sin': 7, 'tan': 7, 'log': 7}
     nested_constr       = {"cos": {"cos": 0}, "sin": {"sin": 0}, "tan": {"tan": 0}, "sin": {"tan": 0}, "cos": {"tan": 0}, "tan": {"cos": 0}, "tan": {"sin": 0}, "cos": {"sin": 0}, "sin": {"cos": 0}}
-  
 
     if node_update == False:
         if const_complexity == 1:
             filename += '_comp1'
         model = pysr.PySRRegressor(
-            model_selection=model_selection,  # Result is mix of simplicity+accuracy
+            model_selection=model_selection, 
             population_size = 100,
             niterations=n_iterations,
             binary_operators=bin_operators,
@@ -75,7 +73,7 @@ def runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations
     
     elif node_update == True:
         msg_model = pysr.PySRRegressor(
-            model_selection=model_selection,  # Result is mix of simplicity+accuracy
+            model_selection=model_selection, 
             population_size = 100,
             niterations=n_iterations,
             binary_operators=bin_operators,
@@ -92,7 +90,7 @@ def runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations
         msg_model.fit(X_train, y_train)
 
         node_model = pysr.PySRRegressor(
-            model_selection=model_selection,  # Result is mix of simplicity+accuracy
+            model_selection=model_selection,
             population_size = 100,
             niterations=n_iterations,
             binary_operators=bin_operators,
@@ -112,7 +110,7 @@ def runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations
         msg_model.refresh()
 
         y_msg_pred = msg_model.predict(X_train)
-        F_msg_pred = np.array([np.sum(y_msg_pred[i*29:(i+1)*29]) for i in range(y_msg_pred.shape[0]//n_neighbors)])
+        F_msg_pred = np.array([np.sum(y_msg_pred[i*n_neighbors:(i+1)*n_neighbors]) for i in range(y_msg_pred.shape[0]//n_neighbors)])
         X_n = np.concatenate((F_msg_pred.reshape((F_msg_pred.shape[0],1)), X_nodeupdate_train), axis = 1)
         X_n = pd.DataFrame(X_n, columns = ['F_i', 'u_x', 'u_y', 'u_z'])
         
@@ -148,12 +146,12 @@ if __name__ == '__main__':
     #Choose between 'accuracy' and 'best' (See PySR documentation for more information on model selection). We used 'best' in this paper. 
     model_selection = 'best'
 
-    X_train, y_train, F_nodeupdate_train, X_nodeupdate_train, X_test, y_test, F_test, X_nodeupdate_test, filename = giveData(Re, phi, spherical, n_neighbors, node_update, const_complexity)
+    X_train, y_train, F_nodeupdate_train, X_nodeupdate_train, X_test, y_test, F_test, X_nodeupdate_test, filename = giveData(Re, phi, spherical, n_neighbors, node_update)
     #Comment the next line if only analysis of a previously trained model is required
-    runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations, run, const_complexity, model_selection, filename)    
+    runGP(X_train, y_train, X_nodeupdate_train, F_nodeupdate_train, n_iterations, run, const_complexity, model_selection, filename, node_update, n_neighbors)    
     
     
-    if (node_update == False) and (const_complexity == 1) :
+    if (node_update == False) and (const_complexity == 1):
         filename += '_comp1'
     if node_update == False:
         model = PySRRegressor.from_file('./GP_results/'+filename+'_equations_'+str(run)+'.pkl', model_selection = 'best')
@@ -161,7 +159,8 @@ if __name__ == '__main__':
         model.refresh()
 
         y_test_pred = model.predict(X_test)
-        F_test_pred = np.array([np.sum(y_test_pred[i*29:(i+1)*29]) for i in range(y_test_pred.shape[0]//n_neighbors)])
+        F_test_pred = np.array([np.sum(y_test_pred[i*n_neighbors:(i+1)*n_neighbors]) for i in range(y_test_pred.shape[0]//n_neighbors)])
+        print('---- Test results for y = f(x) ----')
         print('Test R^2: ', R2(F_test_pred, F_test))
         print('Test MSE: ', MSE(F_test_pred, F_test))
 
@@ -174,9 +173,13 @@ if __name__ == '__main__':
         node_model.refresh()
 
         y_test_msg_pred = msg_model.predict(X_test)
-        F_test_msg_pred = np.array([np.sum(y_test_msg_pred[i*29:(i+1)*29]) for i in range(y_test_msg_pred.shape[0]//n_neighbors)])
+        F_test_msg_pred = np.array([np.sum(y_test_msg_pred[i*n_neighbors:(i+1)*n_neighbors]) for i in range(y_test_msg_pred.shape[0]//n_neighbors)])
         X_n = np.concatenate((F_test_msg_pred.reshape((F_test_msg_pred.shape[0],1)), X_nodeupdate_test), axis = 1)
         X_n = pd.DataFrame(X_n, columns = ['F_i', 'u_x', 'u_y', 'u_z'])
         F_test_node_pred = node_model.predict(X_n)
+        print('---- Test results for y = f(g(x)) ----')
         print('Test R^2: ', R2(F_test_node_pred, F_test))
         print('Test MSE: ', MSE(F_test_node_pred, F_test))
+
+        #Finally, the expert chooses one of the equations from the Pareto front in the equation file (or leave it to the algorithm by setting model_selection = 'best').
+        #Don't forget to refit the constants to the original dataset, since we trained the symbolic model on the output of the graph network.
